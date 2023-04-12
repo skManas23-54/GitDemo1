0c3a6c047743724e7ae587abc08260c025095894
manas <manas@gmail.com>
2023-04-12 18:37:41 +0530

    final commit

--- src/test/java/GitDemoClass.java
+++ src/test/java/GitDemoClass.java
@@ -2,5 +2,6 @@ public class GitDemoClass {
     public static void main(String[] args) {
         System.out.println("this is develop branch");
         System.out.println("modified branch");
+        System.out.println("manas");
     }
 }
